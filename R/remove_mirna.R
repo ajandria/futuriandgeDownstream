438626a11847e568c9c22f10604fafddfb07b958
Adrian Janucik <adrian@MacBook-Pro.local>
2023-07-24 11:46:34 +0200

    everything works but not documented

---
+++ R/remove_mirna.R
@@ -0,0 +1,48 @@
+#' Remove miRNA-related Rows from Count Matrix
+#'
+#' This function filters out rows related to miRNAs in a given count matrix.
+#' The filtering is based on gene identifiers that match those classified as miRNAs
+#' in a provided Gene Transfer Format (GTF) file.
+#'
+#' @param count_matrix A data frame representing the count matrix.
+#' It must contain a column named "Geneid" that includes gene identifiers.
+#' @param gtf_path A character string indicating the path to a GTF file.
+#' This file should contain information about the biotypes of each gene.
+#'
+#' @return A data frame. It represents the count matrix after miRNA-related rows have been removed.
+#' The data frame is sorted by the "Geneid" column in ascending order.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' count_matrix <- data.frame(
+#'   Geneid = c("gene1", "gene2", "gene3"),
+#'   sample1 = c(10, 15, 5),
+#'   sample2 = c(20, 10, 10)
+#' )
+#' gtf_path <- system.file("extdata", "example.gtf", package = "yourPackageName")
+#' remove_mirna(count_matrix, gtf_path)
+#' }
+remove_mirna <- function(count_matrix, organism) {
+  if (organism == "homo_sapiens") {
+    gtf_path <- system.file("extdata", "Homo_sapiens.GRCh38.110.chr.gtf", package = "futuriandgeDownstream")
+  } else if (organism == "mus_musculus") {
+    gtf_path <- system.file("extdata", "Mus_musculus.GRCm39.110.chr.gtf", package = "futuriandgeDownstream")
+  } else {
+    stop(message("Organism not supported..."))
+  }
+
+  # Import GTF and filter for miRNA rows
+  gtf_df_filtered <- rtracklayer::import(gtf_path) %>%
+    as.data.frame() %>%
+    dplyr::select(gene_id, gene_biotype) %>%
+    dplyr::filter(gene_biotype == "miRNA")
+
+  # Define negation of `%in%`
+  `%nin%` <- Negate(`%in%`)
+
+  # Remove miRNA rows and sort by gene ID
+  count_matrix %>%
+    dplyr::filter(Geneid %nin% gtf_df_filtered$gene_id) %>%
+    dplyr::arrange(Geneid)
+}
---
+++ used-locally-for-testing/index.R
@@ -0,0 +1,56 @@
+
+# Setup -------------------------------------------------------------------
+
+downstream_ch_file <- "used-locally-for-testing/counts/extended_source-pipeline-out_meta_file.txt"
+organism <- 'mus_musculus'
+
+user_file <- "used-locally-for-testing/template-user-request.xlsx"
+
+sample_metadata <- readxl::read_excel(user_file, sheet = 'metadata')
+contrasts <- readxl::read_excel(user_file, sheet = 'comparisons')
+
+# Load --------------------------------------------------------------------
+
+# F1
+counts <- return_count_matrix(downstream_ch_file)
+
+# F2
+counts_no_mirnas <- remove_mirna(counts, organism)
+
+# F3
+dge_results <- run_dge(count_matrix_raw = counts_no_mirnas,
+                       metadata_raw = sample_metadata,
+                       comparisons = contrasts)
+
+# save dge results
+save(dge_results, file = 'dge_results.RData')
+
+# F4
+plot_diagnostic_plots(count_matrix_raw = counts_no_mirnas,
+                      metadata_raw = sample_metadata)
+
+# F5
+contrasts_for_report <- contrasts %>%
+  dplyr::mutate(compared_gorups = paste0(
+    studied_effect,
+    '_vs_',
+    baseline
+  ))
+
+for (groups in contrasts_for_report$compared_gorups) {
+
+  sample_metadata$sample <- gsub("_T1",
+                                 "",
+                                 sample_metadata$sample_id)
+
+  render_dge_html_report(
+    comparison_name = groups,
+    dge_results_in = data.frame(dge_results$dge_results[[groups]]),
+    metadata = sample_metadata,
+    norm_counts = dge_results$normalised_counts,
+    raw_counts = dge_results$raw_counts_no_gtf_miRNA,
+    organism = organism,
+    output_name = paste0(groups, ".html")
+  )
+}
+
